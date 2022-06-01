a74afd8b09d4d997b666b8b27c3341fb33fe855b
Nolan Clerc <nolanclerc@orange.fr>
2022-06-01 16:47:58 +0200

    global light, player spawn and level visible and loaded are now done during the createWorld() function

--- scripts/import.py
+++ scripts/import.py
@@ -7,10 +7,16 @@ path_mesh = '/Game/adream/models/meshes'
 path_level = '/Game/adream/maps/'
 
 def createWorld() :
+	
 	unreal.EditorLevelLibrary.new_level(path_level + "Adream")
 	unreal.EditorLevelLibrary.load_level(path_level + "Adream")
 	world = unreal.EditorLevelLibrary.get_editor_world()
 	sub_level_of_world = unreal.EditorLevelUtils.get_levels(world)
+	light=unreal.EditorLevelLibrary.spawn_actor_from_class(unreal.SkyLight,(0,0,1000),(0,0,0))
+	component=unreal.SkyLight()
+	hemishphere_color=unreal.LinearColor(1,1,1,1)	
+	light.light_component.set_editor_property('lower_hemisphere_color', hemishphere_color)
+	light.light_component.set_editor_property('intensity', 10)
 	createLevel(sub_level_of_world)
 
 def createLevel(sub_level_of_world) :	
@@ -20,7 +26,9 @@ def createLevel(sub_level_of_world) :
 		if str(sub_level_of_world).find(path_level + level_name) != -1 :
 			unreal.EditorLevelLibrary.set_current_level_by_name(level_name)
 		else :
-			unreal.EditorLevelUtils.create_new_streaming_level(unreal.LevelStreamingDynamic , new_level_path = path_level + level_name)
+			streaming_level=unreal.EditorLevelUtils.create_new_streaming_level(unreal.LevelStreamingDynamic , new_level_path = path_level + level_name)
+			streaming_level.set_editor_property('initially_loaded',True)
+			streaming_level.set_editor_property('initially_visible',True)
 		createActors(yaml_file[level_name] , '' , level_name , '')
 		unreal.EditorLevelLibrary.save_all_dirty_levels()
 
@@ -36,6 +44,8 @@ def createActors(dic_actor , path_actor_level , actual_dic , path_to_mesh) :
 			, (dic_actor['x']*100,dic_actor['y']*(-100),dic_actor['z']*100), (dic_actor['ry'],dic_actor['rz'],dic_actor['rx']))
 			actor.set_folder_path(path_actor_level)
 			actor.set_actor_label(actual_dic)	
+			if actual_dic== 'IKEAShelf_mesh_002':
+				unreal.EditorLevelLibrary.spawn_actor_from_class(unreal.PlayerStart,(520,-670,180),(0,0,0))
 		
 			
 	else :
